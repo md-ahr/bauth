789ab89970791dceb06356a7f57eeec75a653513
Abdul Halim <ahr.web.pro@gmail.com>
2023-01-16 23:53:44 +0600

    Create routes and components for react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Username from "./components/Username";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Username />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
+    },
+    {
+        path: "/recovery",
+        element: <Recovery />,
+    },
+    {
+        path: "/reset",
+        element: <Reset />,
+    },
+    {
+        path: "*",
+        element: <PageNotFound />,
+    },
+]);
+
 function App() {
-    return <h1 className="my-6 text-3xl font-bold text-center">Welcome To BAuth</h1>;
+    return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
